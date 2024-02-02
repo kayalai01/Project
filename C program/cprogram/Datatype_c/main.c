c53cb2bcd6e54407fd8b7bd4254e6a33482bee87
Malar-M <malarvizhi5869@gmail.com>
2024-02-02 19:56:05 +0530

    add

---
+++ C program/cprogram/Datatype_c/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b,c;
+    char x;
+    double y;
+    float z;
+
+
+    printf("Basic data type \n");
+
+    a=20,b=12,c=15;
+    x= 'Learn abt c programming';
+    y=3.41678;
+    z=1.25;
+
+    printf("%c \n",x);
+    printf("Enter A value :%d \n",a);
+    printf("Enter B value :%d \n",b);
+    printf("Enter C value :%d \n",c);
+    printf("Enter pi value :%d \n",y);
+    printf("Enter pi-2 value :%d \n",z);
+
+
+    return 0;
+}
---
+++ C program/cprogram/operator_c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d;
+    int sum=a+b, sub=a-d, multiply=c*d, division=c/d, modulus=a%d;
+
+    printf ("In this programming Operator With Example !\n\n");
+
+    a=6,b=3,c=8,d=4;
+
+    printf("Sum of value is : %d \n\n", sum);
+    printf("Sub of value is : %d \n\n", sub);
+    printf("multiply of value is : %d\n \n", multiply);
+    printf("Division of value is : %d \n\n", division);
+    printf("Modulus of value is : %d \n\n", modulus);
+    printf("Incerement of value is : %d \n\n", a++);
+    printf("Decerement of value is : %d \n\n", a--);
+
+
+
+
+    printf("Hello world!\n");
+    return 0;
+}
