56aaaeabf2021d93a666511f1a6d5ea3bc89d972
kayal m <kayalai01@brightskills.com>
2024-02-08 00:19:26 -0800

    Add a Varibles file

---
+++ python/pythondemo/third.py
@@ -0,0 +1,32 @@
+'''
+name= "kayal"
+print("Enter your name:",name)
+name=input("Enter your name:")
+print(name)
+
+print("Urugi Urugi ponathadii enn Ureyiee...")
+'''
+
+# declare variables Example
+"""
+x=2024
+y="New year"
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+p=int(13)
+q=str(6)
+r=float(19)
+print(p)
+print(q)
+print(r)
+H=28
+h='hii'
+print(H)
+print(h)
+"""
+x="kayal vizhi"
+print(x)
+x=x.replace("kayal vizhi","malar vizhi")
+print(x)
