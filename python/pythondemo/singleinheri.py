1ff26611ee944a16e1e9e5e45fcc4e57bae7e2d1
kayal m <kayalai01@brightskills.com>
2024-02-14 00:11:58 -0800

    add familytree

--- python/pythondemo/singleinheri.py
+++ python/pythondemo/singleinheri.py
@@ -7,8 +7,21 @@ class grandparents():
     def welcome(self):
      print ( "welcome to ",self.familyname, "wishes from ", self.grandfather, "and" , self.grandmother)
      
-x=grandparents("Nateshan","palambal","Nateshanfam") 
+#x=grandparents("Nateshan","palambal","Nateshanfam") 
+#x.welcome()
+
+class mahendhran(grandparents):
+    def __init__(self,grandpa,grandma,familyname,fathername,mothername):
+     self.fathername=fathername
+     self.mothername=mothername
+     super().__init__(grandpa,grandma,familyname)
+        
+    def thanks(self):
+        print("Hi grandpa", self.grandfather, "and grandma", self.grandmother,"we", self.fathername, "and" , self.mothername, "thank you for warm welcome to our ", self.familyname, "family")
+        
+x=mahendhran("Nateshan","palambal","Nateshanfam","mahendhran","Ragini mahendhran")
 x.welcome()
+x.thanks() 
 
    
       
---
+++ python/sininheritance.py
@@ -0,0 +1,8 @@
+class world()
+    def __init__(self):
+        print("Welcome to world")
+        
+    def continent(self):
+        print("They are seven continent in this world")
+        
+    def ocean(self)
